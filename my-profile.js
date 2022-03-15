7c02855a99a6eb240bb2a370b6d86cd72d2ee74d
Elan Katz <otter2388@gmail.com>
2022-03-15 13:35:35 -0700

    phase 2 complete phase 3 partial

--- my-profile.js
+++ my-profile.js
@@ -1,9 +1,9 @@
 // console.log('It works!')
 
 // const myDocument = document.body;
-const stageOne = document.createElement('h1');
-stageOne.id = 'header';
-stageOne.innerText = "DeadPool\'s Profile"
+const stageOne = document.createElement("h1");
+stageOne.id = "header";
+stageOne.innerText = "DeadPool's Profile";
 document.body.appendChild(stageOne);
 
 // 1A
@@ -20,17 +20,33 @@ document.body.appendChild(stageOne);
 // document.body.append(ul)
 
 //1B
-const arrLi = ['ewedwe','gfdfe','etewte','atge']
+const arrLi = ["ewedfgdwe", "gdfgdffdfe", "etedfgwte", "atdfgdgge"];
 
-const unorderedList = (arr) =>{
-    const ul = document.createElement('ul')
-    arr.forEach(str => {
-    let li = document.createElement('li');
+const unorderedList = (arr) => {
+  const ul = document.createElement("ul");
+  arr.forEach((str) => {
+    let li = document.createElement("li");
     li.innerText = str;
     ul.append(li);
+  });
+
+  document.body.append(ul);
+};
+
+unorderedList(arrLi);
+
+//Phase 2
+document.querySelector("h1").setAttribute("class", "name");
+document.querySelector("ul").setAttribute("class", "my-details");
+const liList = document.querySelectorAll("li");
+liList.forEach((li) => {
+  li.setAttribute("class", "detail");
 });
 
-document.body.append(ul);
-}
+//Phase 3
 
-unorderedList(arrLi)
+const clock = document.createElement("time");
+
+clock.innerText = `${Date.now()}`;
+
+document.body.append(clock);
